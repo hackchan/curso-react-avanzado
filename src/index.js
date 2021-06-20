f916b3ade6912c05d497871b6958783cd18b9ee1
hackchan <fabiorojas7@gmail.com>
2021-06-20 16:51:33 -0500

    se instala linter standard y se configura en el package.json y se sube el proyecto a vercel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-
-
 ReactDom.render(<h1>hola devjs</h1>, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   output: {
     filename: 'app.bundle.js'
   },
-  plugins:[new HtmlWebpackPlugin({
+  plugins: [new HtmlWebpackPlugin({
     template: 'src/index.html'
   })],
 
   module: {
-    rules:[
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use:{
+        use: {
           loader: 'babel-loader',
-          options:{
-            presets:['@babel/preset-env','@babel/preset-react']
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       }
